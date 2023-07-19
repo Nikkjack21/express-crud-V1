e0cd2da662a84c4a4307c6c80bfadf45c750b60b
Nikkjack21 <jackson97patrick@gmail.com>
2023-07-19 20:29:23 +0530

    added env logger

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import logger from "morgan";
+import "dotenv/config.js"
 import routes from "./routes/index.js";
 import { connectDB } from "./db-handlers/db-handler.connection.js";
 
 const app = express();
+
+const { ENVIRONMENT } = process.env;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -11,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 connectDB();
 
 // logger
-app.use(logger("dev"));
+ENVIRONMENT!== 'prod' && app.use(logger("dev"));
 // api routes
 routes(app);
 const { PORT } = process.env;
